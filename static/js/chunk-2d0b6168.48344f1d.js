d669432aee9e5820a0cea96c9fece9ac8aa7dae4
aliyun9805144673 <w_jhao@126.com>
2024-04-17 12:18:19 +0800

    第一次提交

---
+++ static/js/chunk-2d0b6168.48344f1d.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0b6168"],{"1c55":function(t,e,n){"use strict";n.r(e);var s=function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",[n("Details",{attrs:{type:"2"}})],1)},a=[],c=n("6418"),l={components:{Details:c["a"]}},u=l,i=n("2877"),o=Object(i["a"])(u,s,a,!1,null,null,null);e["default"]=o.exports}}]);
